1029c8fff060e7551234b8d55578fc74d6e4c628
meghanhaggard <mlh151@zips.uakron.edu>
2015-10-14 01:45:41 -0400

    plist
    added facts to facts.plist, working on way of accessing info from plist

--- Hungry For Apples?/ViewController.swift
+++ Hungry For Apples?/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func changeButtonTitle(sender: UIButton) {
+    func changeButton(sender: UIButton) {
         // declaring random rgb values for input into UIColor initialization function
         // types are cast from double (drand48() returns type double) to CGFloat
         let red = CGFloat(drand48() % 255)
@@ -29,12 +29,20 @@ class ViewController: UIViewController {
         
         // setting button title color to white
         sender.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        
         // using RGB values to set background color of sender (button) to random color
         sender.layer.backgroundColor = UIColor.init(red: red, green: green, blue: blue, alpha: 0.5).CGColor
+        
+        // holds the path of the plist
+        let path = NSBundle.mainBundle().pathForResource("facts", ofType: "plist")
+        // initializes new dictionary to contents of plist
+        let dictionary = NSDictionary(contentsOfFile: path!)
+        let factsList = dictionary["appleFacts"] as! [AnyObject]
+        
     }
     
     @IBAction func buttonTapped(sender: UIButton) {
-        changeButtonTitle(sender)
+        changeButton(sender)
     }
     
 
