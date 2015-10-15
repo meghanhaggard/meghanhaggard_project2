1ec5aa274c23bd285c2c8b9e8ec450746a686345
meghanhaggard <mlh151@zips.uakron.edu>
2015-10-15 01:38:15 -0400

    cleaned up
    cleaned up comments/spacing

--- Hungry For Apples?/ViewController.swift
+++ Hungry For Apples?/ViewController.swift
@@ -20,9 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // called by IBAction, this function changes the color and text on the button
     func changeButton(sender: UIButton) {
+        
         // declaring random rgb values for input into UIColor initialization function
-        // types are cast from double (drand48() returns type double) to CGFloat
+        // types are cast from double (drand48() returns type double) to CGFloat in order
+        // to use them in our UIColor initialization 
         let red = CGFloat(drand48())
         let green = CGFloat(drand48())
         let blue = CGFloat(drand48())
@@ -33,14 +36,14 @@ class ViewController: UIViewController {
         // using RGB values to set background color of sender (button) to random color
         sender.layer.backgroundColor = UIColor.init(red: red, green: green, blue: blue, alpha: 0.5).CGColor
         
-        // create nsdictionary from facts.plist
+        // create NSDictionary from facts.plist
         var data: Dictionary<String, String>? = {
             guard let path = NSBundle.mainBundle().pathForResource("facts", ofType: "plist") else {
                 print("Invalid path for plist")
                 return nil
             }
             return NSDictionary(contentsOfFile: path) as? Dictionary<String, String>
-            }()
+        }()
         
         // storing our facts from the plist into strings
         let fact1 = data?["fact1"]
@@ -52,6 +55,7 @@ class ViewController: UIViewController {
         // generating random number to display corresponding fact
         let factnumber = (arc4random() % 5) + 1
         
+        // displaying corresponging fact
         if factnumber == 1 {
             sender.setTitle(fact1, forState: UIControlState.Normal)
         }
@@ -67,22 +71,11 @@ class ViewController: UIViewController {
         else {
             sender.setTitle(fact5, forState: UIControlState.Normal)
         }
-        
-        
-        
-        
-        
-        
-        
-        // placeholder for now, will change to facts from plist later
-        
     }
     
+    // what happens when we tap the button?
     @IBAction func buttonTapped(sender: UIButton) {
         changeButton(sender)
     }
-    
-
-
 }
 
